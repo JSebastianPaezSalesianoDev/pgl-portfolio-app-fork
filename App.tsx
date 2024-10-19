efe2aac95473c7db6e376dc06d4935b1bded477f
JSebastianPaezSalesianoDev <usr3766@salesianos-lacuesta.net>
2024-10-19 20:10:25 +0100

    FEAT(AboutMe): Se creo el componente about me que almacenara y sera la estetica de la data sobre mi

--- App.tsx
+++ App.tsx
@@ -3,30 +3,39 @@ import { ScrollView, StyleSheet, Text, View } from "react-native";
 import QRCode from "react-native-qrcode-svg";
 import Header from "./components/Header";
 import Card from "./components/Card";
+import AboutMe from "./components/AboutMe";
 
 export default function App() {
   const [displayMyQR, setDisplayMyQR] = useState(true);
+  const favoriteThings = [
+    "Programar",
+    "Leer libros",
+    "Escuchar música",
+    "Jugar videojuegos",
+    "Viajar",
+  ];
 
   return (
     <View style={styles.container}>
       <Header displayMyQr={() => setDisplayMyQR(true)} />
 
-      {displayMyQR ? (
-        <View style={styles.bodyContainer}>
-          <Card
-            id={22}
-            title="Mi Título"
-            description="Esta es una descripción de ejemplo."
-            imgSource={require("./assets/SofyanAmrabat.jpg")}
-          />
-        </View>
-      ) : (
-        <View style={styles.bodyContainer}>
+      <ScrollView contentContainerStyle={styles.bodyContainer}>
+        {displayMyQR ? (
+          <>
+            <Card
+              id={22}
+              title="Mi Título"
+              description="Esta es una descripción de ejemplo."
+              imgSource={require("./assets/SofyanAmrabat.jpg")}
+            />
+            <AboutMe aboutme={favoriteThings} />
+          </>
+        ) : (
           <View style={styles.centerQRCode}>
             <QRCode value="https://github.com/adhernea" />
           </View>
-        </View>
-      )}
+        )}
+      </ScrollView>
     </View>
   );
 }
@@ -36,37 +45,9 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: "#fff",
     alignItems: "center",
-    justifyContent: "center",
   },
   bodyContainer: {
     width: "100%",
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-    paddingHorizontal: 20,
-  },
-  sectionTitle: {
-    fontSize: 20,
-    fontWeight: "900",
-    textAlign: "center",
-    textTransform: "capitalize",
-    marginVertical: 10,
-  },
-  scrollContainer: {
-    width: "100%",
-  },
-  listItem: {
-    borderColor: "black",
-    borderWidth: 1,
-    borderStyle: "dashed",
-    padding: 15,
-    color: "darkred",
-    textAlign: "center",
-    fontWeight: "bold",
-    fontStyle: "italic",
-    fontSize: 16,
-    backgroundColor: "silver",
-    marginVertical: 5,
   },
   centerQRCode: {
     justifyContent: "center",
--- components/AboutMe.tsx
+++ components/AboutMe.tsx
@@ -1,31 +1,22 @@
 import { ScrollView, StyleSheet, Text, View } from "react-native";
 import React from "react";
 
-const AboutMe = () => {
+export type AboutMeProps = {
+  aboutme: Array<string>;
+};
+
+const AboutMe = ({ aboutme }: AboutMeProps) => {
   return (
-    <view>
+    <View>
       <Text style={styles.sectionTitle}>Cosas que me gustan mucho:</Text>
       <ScrollView style={styles.scrollContainer}>
-        {[
-          "Salir a pasear",
-          "Senderismo",
-          "Ir a la playita",
-          "Domingos de misa",
-          "La guitarrita",
-          "El monte con lluvia",
-          "Viajar",
-          "Música variadita",
-          "Anime",
-          "Ducharme",
-          "Videojuegos",
-          "Ir de cenar romántica",
-        ].map((item, index) => (
+        {aboutme.map((item, index) => (
           <Text key={index} style={styles.listItem}>
             {item}
           </Text>
         ))}
       </ScrollView>
-    </view>
+    </View>
   );
 };
 
