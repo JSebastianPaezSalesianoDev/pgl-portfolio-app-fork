2e1dc75aa8395828beb304c537b10e57a735c9a2
JSebastianPaezSalesianoDev <usr3766@salesianos-lacuesta.net>
2024-10-20 20:21:32 +0100

    Feat(Phrases)se agrego un array de frases para una futura funcion

--- data/AboutMeData.tsx
+++ data/AboutMeData.tsx
@@ -1,5 +1,3 @@
-import { AboutMeProps } from "../components/AboutMe";
-
 export const cards: string[] = [
   "Me gusta programar",
   "Me gusta jugar",
---
+++ data/Phrases.tsx
@@ -0,0 +1,8 @@
+export const phrasesAdrianNeverSaid: string[] = [
+  "La vida es dura pero mas dura es mi verdura",
+  "Para que bajarlas verdes si maduras caen solas (Las frutas)",
+  "ok",
+  "La diferencia entre lo ordinario y lo extraordinario es ese pequeño extra",
+  "El peor de los excesos es no cometer ninguno",
+  "En algun punto todo de la historia todo era imposible, hasta que se intento",
+];
